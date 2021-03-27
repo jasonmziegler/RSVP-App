7fc1f4966202fc37d1668203d34ff1e66cc107f1
Jason Ziegler <jason@insightfulautomation.com>
2021-03-27 17:27:26 -0500

    create function createLI to use in submit event listener

--- 1-tour-the-application/start/js/app.js
+++ 1-tour-the-application/start/js/app.js
@@ -5,11 +5,7 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = input.value;
-    input.value = '';
-    
+function createLI(text) {
     const li = document.createElement('li');
     li.textContent = text;
     const label = document.createElement('label');
@@ -22,6 +18,16 @@ form.addEventListener('submit', (e) => {
     const button = document.createElement('button');
     button.textContent = 'Remove';
     li.appendChild(button);
+    return li;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    
+    const li = createLI(text);
+    
     
     ul.appendChild(li);
 });
