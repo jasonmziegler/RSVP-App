6c4e0ead6c6c75d8e2a97c06094b54dcac054037
Jason Ziegler <jason@insightfulautomation.com>
2021-03-27 16:36:18 -0500

    add event listener to input

--- 1-tour-the-application/start/js/app.js
+++ 1-tour-the-application/start/js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello from app.js');
+// console.log('Hello from app.js');
+
+const form = document.getElementById('registrar');
+
+const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    
+    const li = document.createElement('li');
+    li.textContent = text;
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    label.appendChild(checkbox);
+    li.appendChild(label);
+    ul.appendChild(li);
+});
+
+
+ul.addEventListener('change', (e)=> {
+    console.log(e.target.checked);
+});
